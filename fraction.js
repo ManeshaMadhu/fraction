09d53d010ed09ee4e30c30f28509b02837390ef5
Mershi243 <115769794+Mershi243@users.noreply.github.com>
2023-04-20 11:54:03 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,27 @@
+class addition(){
+  constructor(a,b){
+    this.a=a;
+    this.b=b;
+    this.c=a+b;
+  }
+  get_c(){
+    return(this.c);
+  }
+}
+function input(){
+  a = parseInt(prompt("Enter 1st number");
+  b=parseInt(prompt("Enter 2nd number");
+  return[a,b];
+}
 
+function output(s)
+console.log("The difference between %d and %d,s.a,s.b,s.c);
+}
+function main(){
+  let a=0;
+  let b=0;
+  [a,b] = input();
+  let s = new addition(a,b)
+  output(s);
+}
+            
